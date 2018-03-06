71cb6b468661e0aca23deae6680c4068de9990a1
KristianND <dikandapamerang2000@gmail.com>
2018-03-07 02:25:56 +0700

    Pengeditan file index.html dan .php

--- email_sending_php.php
+++ email_sending_php.php
@@ -12,8 +12,8 @@ $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
 
 // More headers
-$headers .= 'From: Jurnalweb.com <noreply@yourwebsite.com>'."\r\n" . 'Reply-To: '.$name.' <'.$email.'>'."\r\n";
-$headers .= 'Cc: '. $email . "\r\n"; //untuk cc lebih dari satu tinggal kasih koma
+$headers .= "From : ".$email."\r\n" . 'Reply-To: '.$name.' <'.$email.'>'."\r\n";
+$headers .= "Cc: ".$email. "\r\n"; //untuk cc lebih dari satu tinggal kasih koma
 @mail($to,$subject,$message,$headers);
 if(@mail)
 {
