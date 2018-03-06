7de6516c4196ea48ef32786a793dde4a7e90ac3a
KristianND <dikandapamerang2000@gmail.com>
2018-03-06 17:49:34 +0700

    Update Baru

--- email_sending_php.php
+++ email_sending_php.php
@@ -4,13 +4,16 @@ $email=$_POST['email'];
 $subject=$_POST['subject'];
 $message=$_POST['message'];
 
-$to='teknologi.informasi@its.ac.id';
+$to='dikandapamerang@gmail.com';
 
 $message="From:$name <br />".$message;
 
 $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
 
+// More headers
+$headers .= 'From: Jurnalweb.com <noreply@yourwebsite.com>'."\r\n" . 'Reply-To: '.$name.' <'.$email.'>'."\r\n";
+$headers .= 'Cc: '. $email . "\r\n"; //untuk cc lebih dari satu tinggal kasih koma
 @mail($to,$subject,$message,$headers);
 if(@mail)
 {
