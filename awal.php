e323001c6855d93a0995cafaef27d805fc9665c0
KristianND <dikandapamerang2000@gmail.com>
2018-03-13 23:50:05 +0700

    Version 4.1
    Pengeditan page awal.php

--- awal.php
+++ awal.php
@@ -8,15 +8,16 @@ $result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC");
  
 <html>
 <head>    
-    <title>Homepage</title>
+    <title>Admin Page</title>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<link rel="stylesheet" src="lib\bootstrap\css\bootstrap.css"></link>
+	<link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Montserrat:300,400,500,700" rel="stylesheet">
 </head>
  
 <body>
 <div id="mtable" class="jumbotron">
 <a href="http://kindika-ndapamerang.000webhostapp.com/">Go back to webpage</a><br/><br/>
- 
+	<h3 style="text-align: center;">"Respondent Page"</h3>
     <table width='80%' border=1>
  
     <tr>
@@ -34,22 +35,45 @@ $result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC");
     ?>
     </table>
 	</div>
-	<div id="form">
-		<input id="username" type='text'> </input>
-		<input id="password" type='text'> </input>
-		<button onClick="admin()">Click</button>
-	</div>
+	
+	<section id="contact" class="section-bg wow fadeInUp" style="border: 2px solid red; border-radius: 12px; margin: 180px; font-family:courier;">
+		<div id="form">
+		
+		<br>
+		<h3 class="text-center"><strong>Admin Login Page</strong></h3>
+		<h5 class="text-center">Please Enter Username and Password</h5>
+		<div class="container">
+
+        
+	    <div class="form-row">
+			<br><br><br>
+			<div class="form-group col-md-6">
+				<input type="text" class="form-control" name="name" data-rule="minlen:4" id="username" placeholder="Username">
+			</div>
+
+			<div class="form-group col-md-6">
+				<input type="text" class="form-control" name="email" data-rule="email" id="password" placeholder="Password">
+			</div>
+		</div>
+		<div class="text-center"><button onClick="admin()">Log In</button></div>
+		<br><br>
+		<p><em>*If you are not admin, please consider to leave this page immediately</em></p>
+		<a href="http://kindika-ndapamerang.000webhostapp.com/"><em>Go back to Information Technology Webpage</em></a>
+		</div>
+		</div>
+    </section>
+	
 	<script>
 		$('#mtable').hide();
 		
 		function admin(){
-			if($('#username').val()==='admin'&& $('#password').val()==='123456'){
+			if($('#username').val()==='admin'&& $('#password').val()==='admin'){
 					
 			$('#mtable').show();
 			$('#form').hide();
 			}
 			else{
-				window.alert('you are not admin');
+				window.alert('Username Or Password Incorrect');
 			}
 		}
 	</script>
