18a992343e70e65568cf40d8c850f0d5599a22c0
KristianND <dikandapamerang2000@gmail.com>
2018-03-13 15:32:46 +0700

    Version 3
    Penambahan database. Ketika pengunjung mengirimkan email, data yang dinputkan akan tersimpan pula pada data base yang terhubung

--- email_sending_php.php
+++ email_sending_php.php
@@ -18,6 +18,31 @@ $headers .= "Cc: ".$email. "\r\n"; //untuk cc lebih dari satu tinggal kasih koma
 @mail($to,$subject,$message,$headers);
 if(@mail)
 {
-   echo "Email sent successfully !!";	
+	echo : sukses;
 }
+
+
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "database";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO data (name, email, subject, message)
+VALUES ('".$name."', '".$email."', '".$subject."', '".$message."')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
 ?>
